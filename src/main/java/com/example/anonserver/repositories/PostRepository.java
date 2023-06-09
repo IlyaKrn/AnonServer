6b86b384a9de379bff88bae4d9b3f3217ad1ad3e
IlyaKrn <ilyakornoukhov@gmail.com>
2023-06-09 23:58:10 +0300

    post public

--- src/main/java/com/example/anonserver/repositories/PostRepository.java
+++ src/main/java/com/example/anonserver/repositories/PostRepository.java
@@ -7,6 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<PostModel, Long> {
-    List<PostModel> findByIsBanned(boolean isBanned);
-    List<PostModel> findByAuthorId(long authorId);
 }
