6fc44d06f9abcfb3ac9e2e40ce03256ae0bbfa62
IlyaKrn <ilyakornoukhov@gmail.com>
2023-06-10 21:27:00 +0300

    bugfix

--- src/main/java/com/example/anonserver/api/AuthController.java
+++ src/main/java/com/example/anonserver/api/AuthController.java
@@ -6,6 +6,7 @@ import com.example.anonserver.jwt.models.JwtRequest;
 import com.example.anonserver.jwt.models.JwtResponse;
 import com.example.anonserver.jwt.models.RefreshJwtRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,20 +24,32 @@ public class AuthController {
 
     @PostMapping("login")
     public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest authRequest) throws AuthException {
-        final JwtResponse token = authService.login(authRequest);
-        return ResponseEntity.ok(token);
+        try {
+            final JwtResponse token = authService.login(authRequest);
+            return ResponseEntity.ok(token);
+        } catch (AuthException e){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
     }
 
     @PostMapping("token")
     public ResponseEntity<JwtResponse> getNewAccessToken(@RequestBody RefreshJwtRequest request) throws AuthException {
-        final JwtResponse token = authService.getAccessToken(request.getRefreshToken());
-        return ResponseEntity.ok(token);
+        try {
+            final JwtResponse token = authService.getAccessToken(request.getRefreshToken());
+            return ResponseEntity.ok(token);
+        } catch (AuthException e){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
     }
 
     @PostMapping("refresh")
     public ResponseEntity<JwtResponse> getNewRefreshToken(@RequestBody RefreshJwtRequest request) throws AuthException {
-        final JwtResponse token = authService.refresh(request.getRefreshToken());
-        return ResponseEntity.ok(token);
+        try {
+            final JwtResponse token = authService.refresh(request.getRefreshToken());
+            return ResponseEntity.ok(token);
+        } catch (AuthException e){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
     }
 
 }
